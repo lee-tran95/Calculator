905a732d0f399d462e92d95aeff3ba4b7edf6958
Lee Tran <lee.tran95@yahoo.com>
2022-06-07 22:29:53 -0700

    Add comments and handled division by 0

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let numbersArray = [];
 num1 = [];
 num2 = [];
 let operator = '';
+let decimal = false;
 
 function add (num1,num2){
     return [+num1 + +num2];
@@ -28,8 +29,21 @@ function operate(operator, num1, num2){
         case '*':
             return multiply(num1,num2);
         case '/':
-            return divide(num1,num2)
+            if(num2 == 0){
+                console.log("Deleting all data, can't divide by zero")
+                clear();
+            }else{
+                return divide(num1,num2)
+            }      
     }
+}
+
+function clear(){
+    numbersArray = [];
+    num1 = [];
+    num2 = [];
+    operator = '';
+    document.querySelector('.screen').innerText = 0;
 }
 
 
@@ -44,6 +58,8 @@ function displayScreen(array){
     })
 }
 
+//let's user enter numbers that is stored in an array while also
+//displaying the number onto the screen
 let numberButtons = document.querySelectorAll(".number")
 numberButtons.forEach(number =>{
     number.addEventListener('click', () =>{
@@ -54,20 +70,36 @@ numberButtons.forEach(number =>{
     })
 })
 
+//if a user press an operator button, the operation is stored in
+//an operator variable and the current numbers array is stored in num1
+//if the user presses the operator while num1 exists and there are
+//numbers in the numbers array, it will perform the existing operation
 let operatorButton = document.querySelectorAll('.operator');
 operatorButton.forEach(item =>{
     item.addEventListener('click',() =>{
+        if(num1.length > 0 && numbersArray.length > 0){
+            num2 = numbersArray.join('');
+            result = operate(operator,num1,num2)
+            displayScreen(result);
+            num1 = result.join('');
+            numbersArray = [];
+            operator = item.innerText;
+        }else{
         operator = item.innerText;
         num1 = numbersArray.join('');
         numbersArray = [];
-        if(!num1 && !numbersArray){
-            
         }
     })
 })
 
+//calculates and displays the results of the two numbers and the operator chosen
 let equal = document.querySelector('.operate');
 equal.addEventListener('click', () =>{
-    num2 = numbersArray.join('');
-    displayScreen((operate(operator,num1,num2)))
+    if(num1.length > 0 && operator!== '' && numbersArray.length >0){
+        num2 = numbersArray.join('');
+        let result = operate(operator,num1,num2)
+        if(result) displayScreen(result)
+    }
 })
+
+document.querySelector('.clear').addEventListener('click', clear)
