a35edeeb6eb0f6117e0774be4a90b479be4036b6
Lee Tran <lee.tran95@yahoo.com>
2022-06-07 21:56:43 -0700

    Add event listeners to each operator and equal sign

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 let numbersArray = [];
+num1 = [];
+num2 = [];
+let operator = '';
 
 function add (num1,num2){
-    return num1 + num2;
+    return [+num1 + +num2];
 }
 
 function subtract(num1,num2){
-    return num1 - num2;
+    return [+num1 - +num2];
 }
 
 function multiply(num1,num2){
-    return num1 * num2;
+    return [+num1 * +num2];
 }
 
 function divide(num1,num2){
-    return num1 / num2
+    return [+num1 / +num2];
 }
 
 function operate(operator, num1, num2){
@@ -40,10 +43,31 @@ function displayScreen(array){
         return screen.innerText += item
     })
 }
+
 let numberButtons = document.querySelectorAll(".number")
 numberButtons.forEach(number =>{
     number.addEventListener('click', () =>{
+        if(numbersArray.length < 15){
         numbersArray.push(number.innerText)
         displayScreen(numbersArray);
+        }
     })
+})
+
+let operatorButton = document.querySelectorAll('.operator');
+operatorButton.forEach(item =>{
+    item.addEventListener('click',() =>{
+        operator = item.innerText;
+        num1 = numbersArray.join('');
+        numbersArray = [];
+        if(!num1 && !numbersArray){
+            
+        }
+    })
+})
+
+let equal = document.querySelector('.operate');
+equal.addEventListener('click', () =>{
+    num2 = numbersArray.join('');
+    displayScreen((operate(operator,num1,num2)))
 })
